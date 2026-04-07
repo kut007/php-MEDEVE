8d0a34cbce52488f2e97e2214176504db0bf1670
kut007 <antonio.rita.fer@gmail.com>
2026-04-07 08:44:22 -0300

    boostrap quase concluido

---
+++ CadastrarAlunoRaiz/index.php/index.php
@@ -0,0 +1,29 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <title>Document</title>
+</head>
+<body>
+    <button type="button" class="btn btn-danger">Vermelho</button>
+    <button type="button" class="btn btn-success">Verde</button>
+
+    <form>
+  <div class="form-group">
+    <label for="formGroupExampleInput">Nome</label>
+    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Nome">
+  </div>
+  <div class="form-group">
+    <label for="formGroupExampleInput2">Email</label>
+    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Email">
+  </div>
+</form>
+    
+</body>
+</html>

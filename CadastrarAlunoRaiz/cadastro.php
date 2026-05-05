074406fe17777029bd47c6382a690df18ffde2cc
unknown <antonio.rita.fer@gmail.com>
2026-05-05 11:44:51 -0300

    repository feito, porem passando apenas 2 de 3 informaçoes

---
+++ CadastrarAlunoRaiz/cadastro.php
@@ -0,0 +1,37 @@
+<?php 
+include "config/config.php"; 
+require_once __DIR__ . '/src/repository.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nome = $_POST['nome'] ?? '';
+    $senha = $_POST['senha'] ?? '';
+
+    cadastro_usuario($nome, $senha);
+    echo "salvo!";
+}
+
+?>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<h2>CADASTRO MEDEVE!!!!!!!</h2>
+
+<form method="POST">
+  <div class="form-group row">
+    <label for="email" class="col-sm-2 col-form-label">Nome</label>
+    <div class="col-sm-10">
+      <input type="text" name="nome" class="form-control" id="email" placeholder="Gmail">
+    </div>
+  </div>
+
+  <div class="form-group row mt-3">
+    <label for="senha" class="col-sm-2 col-form-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" name="senha" class="form-control" id="senha" placeholder="Password">
+    </div>
+  </div>
+
+  <div class="mt-3">
+    <button type="submit" class="btn btn-primary">Cadastrar</button>
+  </div>
+</form>
+
--- CadastrarAlunoRaiz/src/repository.php
+++ CadastrarAlunoRaiz/src/repository.php
@@ -6,35 +6,50 @@ function listar_usuarios() : array
    $sql = "SELECT * FROM usuario ORDER BY id_usuario DESC";
 return db()->query($sql)->fetchAll();
 }
-
-function criar_conta(string $descricao, ?string $tipo, int $valor): int
+function login_usuario(string $nome, string $perfil, string $senha ) : array
 {
-    $sql = "INSERT INTO tarefas (titulo, descricao, status VALUES (?,?,?)";
+    $sql = "SELECT nome, perfil, senha FROM usuario WHERE nome = ?";
     $stmt = db()->prepare($sql);
-    $stmt->execute([$titulo, $descricao, $status]);
+    $stmt->execute([$nome]);
+    $usuario = $stmt->fetch();
+    return $usuario ?: [];
+}
+function cadastro_usuario(string $nome, string $senha, ?string $perfil) : int
+{
+    $sql = "INSERT INTO usuario (nome, senha) VALUES (?,?)";
+    $stmt = db()->prepare($sql);
+    $stmt->execute([$nome, $senha]);
     return (int) db()->lastInsertId();
 }
 
-function buscar_conta(int $id) : ?array
+function criar_conta(int $id_usuario, string $descricao, string $tipo, float $valor): int
 {
-    $sql = "SELECT * FROM tarefas WHERE ID= ?";
+    $sql = "INSERT INTO contas (id_usuario, tipo, descricao, valor) VALUES (?,?,?,?)";
     $stmt = db()->prepare($sql);
-    $stmt->execute([$id]);
+    $stmt->execute([$id_usuario, $descricao, $tipo, $valor]);
+    return (int) db()->lastInsertId();
+}
+
+function buscar_conta(int $id_conta) : ?array
+{
+    $sql = "SELECT * FROM contas WHERE id_conta= ?";
+    $stmt = db()->prepare($sql);
+    $stmt->execute([$id_conta]);
     $row = $stmt->fetch();
      return $row ?: null;
 }
 
-function atualizar_conta(int $id, string $titulo, ?string $descricao, string $status) : bool 
+function atualizar_conta(int $id_conta, string $tipo, string $descricao, float $valor) : bool 
 {
-    $sql = "UPDATE tarefas SET titulo = ?, descricao = ?, status = ? WHERE id = ?";
+    $sql = "UPDATE contas SET tipo = ?, descricao = ?, valor = ? WHERE id_conta = ?";
     $stmt = db()->prepare($sql);
-    return $stmt->execute([$titulo, $descricao, $status, $id]); 
+    return $stmt->execute([$tipo, $descricao, $valor, $id_conta]); 
 }
 
-function excluir_conta(int $id) 
+function excluir_conta(int $id_conta) 
 {
-    $sql = "DELETE FROM tarefas WHERE id = ?";
+    $sql = "DELETE FROM contas WHERE id_conta = ?";
     $stmt = db()->prepare($sql);
-    return $stmt->execute([$id]);
+    return $stmt->execute([$id_conta]);
 }
-?>
+?> 
